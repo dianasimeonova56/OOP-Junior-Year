e6d02d6babba6de75053aeec674d616aaf9abd51
Diana Simeonova <122699903+dianasimeonova56@users.noreply.github.com>
2023-10-26 23:42:32 +0300

    Add files via upload

---
+++ printInFile/ConsoleApplication16.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	string namefile = "d:\data\\text.txt";
+	fstream fp;
+	fp.open(namefile, ios::in);
+	string line, toPrint;
+	while (!fp.eof()) {
+		getline(fp, line);
+		if (fp.eof()) {
+			toPrint += line + ".";
+		}
+		else {
+			toPrint += line + " ";
+		}
+	}
+	cout << toPrint;
+	fp.close()
+}
